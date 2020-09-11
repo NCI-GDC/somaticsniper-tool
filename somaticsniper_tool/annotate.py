8e870c3198abf2783c3a421cca1becdfdaac7702
Charles Czysz <czysz@uchicago.edu>
2020-09-11 08:30:40 -0500

    Update code for annotating vcf

--- somaticsniper_tool/annotate.py
+++ somaticsniper_tool/annotate.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
+import csv
 from enum import Enum
+from types import SimpleNamespace
+
+DI = SimpleNamespace(csv=csv, open=open)
 
 
 class Filter(Enum):
@@ -9,34 +13,45 @@ class Filter(Enum):
     REJECT = """##FILTER=<ID=REJECT,Description="Rejected as an unconfident somatic mutation">"""
 
 
-def annotate_filter(raw, post_filter, new):
-    """Annotate post filter vcf"""
-    writer = open(new, "w")
-    r = open(raw)
-    filtered = open(post_filter)
-    hc = set(r).intersection(filtered)
-    r.close()
-    filtered.close()
-    try:
-        with open(raw) as f:
-            for line in f:
+class Annotate:
+    def __init__(self, output_file: str, _di=DI):
+        self.output_file = output_file
+        self._di = _di
+
+        self.output_fh = None
+
+    def __call__(self, *args, **kwargs):
+        return self.annotate(*args, **kwargs)
+
+    def __enter__(self):
+        self.output_fh = self._di.open(self.output_file, 'w')
+        return self
+
+    def __exit__(self):
+        self.output_fh.close()
+
+    def annotate(self, raw_vcf: str, post_filter: str, _di=DI):
+        """Annotate post filter vcf"""
+        with open(raw_vcf, 'r') as vcf_fh, open(post_filter, 'r') as post_fh:
+            hc = set(vcf_fh).intersection(post_fh)
+
+        with open(raw_vcf, 'r') as vcf_fh:
+            for line in vcf_fh:
                 if line.startswith("##reference"):
-                    writer.write(line)
-                    writer.write("{}\n".format(filter_pass))
-                    writer.write("{}\n".format(filter_reject))
-                    writer.write("{}\n".format(filter_loh))
+                    self.output_fh.write()
+                    self.output_fh.write("{}\n".format(Filter.PASS.value))
+                    self.output_fh.write("{}\n".format(Filter.REJECT.value))
+                    self.output_fh.write("{}\n".format(Filter.LOH.value))
                 elif line.startswith("#"):
-                    writer.write(line)
+                    self.output_fh.write(line)
                 elif line in hc:
-                    new = line.split("\t")
-                    new[6] = "LOH"
-                    writer.write("\t".join(new))
+                    entries = line.split("\t")
+                    entries[6] = "LOH"
+                    self.output_fh.output_fh.write('\t'.join(entries))
                 else:
-                    new = line.split("\t")
-                    new[6] = "REJECT"
-                    writer.write("\t".join(new))
-    finally:
-        writer.close()
+                    entries = line.split("\t")
+                    entries[6] = "REJECT"
+                    self.output_fh.write("\t".join(entries))
 
 
 # __END__
