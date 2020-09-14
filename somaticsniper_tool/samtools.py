725e76c58ec97ceb782fdad818eb3a077ea5f2b3
Charles Czysz <czysz@uchicago.edu>
2020-09-14 09:07:19 -0500

    DI Tempfile

--- somaticsniper_tool/samtools.py
+++ somaticsniper_tool/samtools.py
@@ -2,10 +2,13 @@
 
 import logging
 import tempfile
+from types import SimpleNamespace
 
 from somaticsniper_tool import utils
 
 logger = logging.getLogger(__name__)
+
+DI = SimpleNamespace(tempfile=tempfile)
 
 
 class SamtoolsView:
@@ -13,24 +16,21 @@ class SamtoolsView:
 
     COMMAND_STR = "samtools view -b {bam_path} {region}"
 
-    def __init__(self, bam_file: str, region: str, _utils=utils):
+    def __init__(self, bam_file: str, region: str, _utils=utils, _di=DI):
         self.bam_file = bam_file
         self.region = region
 
         self._utils = _utils
 
-        self.temp_view_fh = tempfile.NamedTemporaryFile()
+        self.temp_view_fh = _di.tempfile.NamedTemporaryFile()
         self.temp_view_name = self.temp_view_fh.name
 
     def __enter__(self):
-        try:
-            self.write_view()
-        except Exception:
-            raise
+        self.write_view()
         return self.temp_view_name
 
     def __exit__(self, type, value, traceback):
-        self.temp_view_fio.close()
+        self.temp_view_fh.close()
 
     def write_view(self):
         cmd = self.COMMAND_STR.format(bam_path=self.bam_file, region=self.region)
